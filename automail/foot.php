b6646db94c3bf34495e24ca1fd06967f373233f2
me-suzy <me.suzana@gmail.com>
2025-08-09 21:23:49 +0300

    Add automail/foot.php

---
+++ automail/foot.php
@@ -0,0 +1,6 @@
+<?
+echo <<<EOT
+<div align="right"><font face="Verdana,Arial" size="2">Powered by <a href="http://www.phpselect.com/" target="_blank">AutoMail</a>. 
+EOT;
+echo "</font></div>";
+?>
