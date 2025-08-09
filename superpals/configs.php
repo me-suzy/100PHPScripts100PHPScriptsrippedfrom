dff972c555d56cc25077c3821f68117fc1791d1e
me-suzy <me.suzana@gmail.com>
2025-08-09 22:35:08 +0300

    Add superpals/configs.php

---
+++ superpals/configs.php
@@ -0,0 +1,48 @@
+<?
+#########################################################
+#                     SuperPals                         #
+#########################################################
+#                                                       #
+#                                                       #
+# This script was created by:                           #
+#                                                       #
+# PHPSelect Web Development Division.                   #
+# http://www.phpselect.com/                             #
+#                                                       #
+# This script and all included modules, lists or        #
+# images, documentation are copyright 2002              #
+# PHPSelect (http://www.phpselect.com/) unless          #
+# otherwise stated in the script.                       #
+#                                                       #
+# Purchasers are granted rights to use this script      #
+# on any site they own. There is no individual site     #
+# license needed per site.                              #
+#                                                       #
+# Any copying, distribution, modification with          #
+# intent to distribute as new code will result          #
+# in immediate loss of your rights to use this          #
+# program as well as possible legal action.             #
+#                                                       #
+# This and many other fine scripts are available at     #
+# the above website or by emailing the authors at       #
+# admin@phpselect.com or info@phpselect.com             #
+#                                                       #
+#########################################################
+
+
+# Web Address (URL) of the pals.php script
+$scripturl = "http://box/shaun/superpals/pals.php";
+
+# Address to send admin mails to, if mail_admin is set to yes below. 
+$admin_email = "s@shat.net";
+
+# Do we send an email to admin upon new signups? (yes or no)
+$mail_admin = "yes";
+
+# Do we send an email to user upon new signup? (yes or no)
+$mail_user = "yes";
+
+
+# Set an administration password.
+$adminpass = "pass";
+?>
