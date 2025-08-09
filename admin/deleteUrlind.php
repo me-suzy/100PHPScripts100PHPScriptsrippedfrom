70678c6c97cb6a98d549115619a03afdbe468e35
me-suzy <me.suzana@gmail.com>
2025-08-09 20:54:50 +0300

    Add admin/deleteUrlind.php

---
+++ admin/deleteUrlind.php
@@ -0,0 +1,124 @@
+<?php require_once('Connections/dbConnect.php'); ?>
+<?php
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+
+if ((isset($HTTP_GET_VARS['id'])) && ($HTTP_GET_VARS['id'] != "") && (isset($HTTP_POST_VARS['submit']))) {
+  $deleteSQL = sprintf("DELETE FROM url_table WHERE id=%s",
+                       GetSQLValueString($HTTP_GET_VARS['id'], "int"));
+
+  mysql_select_db($database_dbConnect, $dbConnect);
+  $Result1 = mysql_query($deleteSQL, $dbConnect) or die(mysql_error());
+
+  $deleteGoTo = "urlListInd.php";
+  if (isset($HTTP_SERVER_VARS['QUERY_STRING'])) {
+    $deleteGoTo .= (strpos($deleteGoTo, '?')) ? "&" : "?";
+    $deleteGoTo .= $HTTP_SERVER_VARS['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $deleteGoTo));
+}
+
+$colname_Recordset1 = "1";
+if (isset($HTTP_GET_VARS['id'])) {
+  $colname_Recordset1 = (get_magic_quotes_gpc()) ? $HTTP_GET_VARS['id'] : addslashes($HTTP_GET_VARS['id']);
+}
+mysql_select_db($database_dbConnect, $dbConnect);
+$query_Recordset1 = sprintf("SELECT * FROM url_table WHERE id = %s", $colname_Recordset1);
+$Recordset1 = mysql_query($query_Recordset1, $dbConnect) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+?>
+
+<html>
+ <head>
+  <title>
+   Hitjammer 1.0  </title>
+ 
+<link href="default.css" rel="stylesheet" type="text/css">
+</head>
+
+<META content="text/html; charset=windows-1252" http-equiv=Content-Type>
+
+ 
+<body bgcolor="#000000">
+<table bgcolor="WHITE" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr> 
+    <td width="800"> 
+      <!---  top menu --->
+      <TABLE bgColor="WHITE" border="0" cellPadding="0" cellSpacing="0" width="800">
+        <TBODY>
+          <TR> 
+            <TD bgColor="WHITE" width="75"></TD>
+            <TD  width="650"><IMG border="0" height="72" src="images/logo.gif" width="650" alt="Hitjammer 1.0"></TD>
+            <TD bgColor="WHITE" width="75">&nbsp;</TD>
+          </TR>
+          <TR> 
+            <TD bgColor="WHITE" width=75></TD>
+            <TD align="CENTER" bgColor="WHITE" height=20 vAlign="MIDDLE" width="650"> 
+              <P align=left ><span class="smallstatsText">Hitjammer 1.0 </span><IMG border=0 height=2 src="images/linie.gif" width="650" alt="--"></P></TD>
+            <TD bgColor="WHITE" width=75>&nbsp;</TD>
+          </TR>
+        </TBODY>
+      </TABLE>
+      <!---  top menu end --->
+    </td>
+  </tr>
+  <tr> 
+    <td width="800"> <table width="100%" border="0" align="center" cellpadding="4">
+        <tr> 
+          <td width="20%" align="center" valign="top"> <div align="center"> <br>
+              <br>
+            </div></td>
+          <td width="61%" valign="top"> <font color="#CCCCCC">&nbsp; </font>
+            <p class="menuheader">Are you sure you want to delete this url?</p>
+            <form name="form1" method="post" action="">
+              <input name="submit" type="hidden" value="true">
+              <input name="submit2" type="submit" id="submit" value="delete">
+            </form>
+            <p><a href="urlListInd.php?id=<?php print $_GET['id']; ?>&return=<?php print $_GET['return'] ?>" class="smallstatsText">&lt;&lt;NO! 
+              Go back</a></p>
+</TD>
+          <td width="19%" valign="top">&nbsp; </TD>
+        </TR>
+      </TABLE>
+      <!-- END body area --->
+  </tr>
+  <tr> 
+    <td width="800" align="center"> 
+      <table>
+<TR>
+                  
+          <td class="smallstatsText">&copy;2002 <a href="http://www.mishies.com" target="new">Mishies.com</a></td>
+</tr>
+ </table>
+     </td>
+  </tr>
+</table>
+</body>
+</html>
+<?php
+mysql_free_result($Recordset1);
+?>
+
