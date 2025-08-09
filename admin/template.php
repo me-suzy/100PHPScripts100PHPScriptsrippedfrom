4acb85f98f51e6cc746c6494ebee31c2cef17674
me-suzy <me.suzana@gmail.com>
2025-08-09 21:01:41 +0300

    Add admin/template.php

---
+++ admin/template.php
@@ -0,0 +1,21 @@
+<? 
+require 'include.php'; 
+pageHeader();
+?>
+<!-- start main template -->
+<table width="100%" border="0">
+  <tr align="center" valign="top"> 
+    <td colspan="3">LOGO</td>
+  </tr>
+  <tr> 
+    <td width="20%" align="left" valign="top"><? loginBlock(); mainMenu(); ?>LEFT BLOCK</td>
+    <td width="67%" align="default" valign="top"><? newsBlock(); ?>CENTER BODY</td>
+    <td width="20%" align="right" valign="top"><? contentBlock(); ?>RIGHT BLOCK</td>
+  </tr>
+  <tr align="center" valign="bottom"> 
+    <td colspan="3"><? footer(); ?>FOOTER</td>
+  </tr>
+</table>
+<!-- end main template -->
+</body>
+</html>
