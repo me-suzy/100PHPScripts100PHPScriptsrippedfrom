4d8f68f99daa03a333ce67ab2fd72c1bbef96b70
me-suzy <me.suzana@gmail.com>
2025-08-09 20:52:19 +0300

    Add admin/adminlisttemplate.php

---
+++ admin/adminlisttemplate.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="default.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<span class="smallstatsText">Records to of 3</span><br>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr> 
+    <td class="menuheader">title</td>
+    <td class="menuheader">title</td>
+    <td class="menuheader">title</td>
+    <td class="menuheader">title</td>
+    <td width="5%" class="menuheaderAction">action</td>
+  </tr>
+  <tr> 
+    <td class="smallstatsText">data</td>
+    <td class="smallstatsText">data</td>
+    <td class="smallstatsText">data</td>
+    <td class="smallstatsText">data</td>
+    <td class="smalltext">options</td>
+  </tr>
+  <tr> 
+    <td colspan="5" align="center" class="smalltext">first prev next last</td>
+  </tr>
+</table>
+</body>
+</html>
